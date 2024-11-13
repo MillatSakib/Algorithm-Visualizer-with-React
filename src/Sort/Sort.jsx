a77f820e248b7d4ea31488289b98d4973f22dda7
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-13 21:41:14 +0600

    sort page completed

--- src/Sort/Sort.jsx
+++ src/Sort/Sort.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Card from "./Card";
+import bubble from "../assets/bubble.jpeg";
+import counting from "../assets/counting.jpg";
+import selection from "../assets/selection.jpeg";
 
 const Sort = () => {
-  return <div>This is sort Page</div>;
+  return (
+    <div>
+      <div className="grid gird-cols-1 md:flex gap-8 items-center justify-center">
+        <Card
+          title="Bubble Sort"
+          description="The Easiest Sorting Algorithm.f"
+          linkTo="/sort/bubblesort"
+          imgLink={bubble}
+        ></Card>{" "}
+        <Card
+          title="Counting Sort"
+          description="The effcicent sorting algorithm for Integer number."
+          linkTo="/sort/countingsort"
+          imgLink={counting}
+        ></Card>
+      </div>
+      <div className="mt-8 grid gird-cols-1 md:flex gap-8 items-center justify-center">
+        <Card
+          title="Selection Sort"
+          description="Efficient for Small Dataset, Limited memory and Real time processing."
+          linkTo="/sort/selectionsort"
+          imgLink={selection}
+        ></Card>
+      </div>
+    </div>
+  );
 };
 
 export default Sort;
