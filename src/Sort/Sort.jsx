ed78ef474befdb336ce3bc6e7811db45ecddfdd8
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-13 20:36:35 +0600

    404 bug fix

---
+++ src/Sort/Sort.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sort = () => {
+  return <div>This is sort Page</div>;
+};
+
+export default Sort;
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,27 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Layout";
 import Main from "./Main/Main";
+import ErrorPage from "./ErrorPage/ErrorPage.jsx";
+import Search from "./Search/Search.jsx";
+import Sort from "./Sort/Sort.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
-    errorElement: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
         element: <Main></Main>,
+      },
+      {
+        path: "/sort",
+        element: <Sort />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
       },
     ],
   },
