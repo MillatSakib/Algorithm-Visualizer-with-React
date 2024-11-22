2b7e8a793708692e0d89529638b6fa206c4e752d
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-22 20:01:31 +0600

    Counting sort removed

--- src/Sort/Sort.jsx
+++ src/Sort/Sort.jsx
@@ -10,18 +10,10 @@ const Sort = () => {
       <div className="grid gird-cols-1 md:flex gap-8 items-center justify-center">
         <Card
           title="Bubble Sort"
-          description="The Easiest Sorting Algorithm.f"
+          description="The Easiest Sorting Algorithm for Beginner."
           linkTo="/sort/bubblesort"
           imgLink={bubble}
         ></Card>{" "}
-        <Card
-          title="Counting Sort"
-          description="The effcicent sorting algorithm for Integer number."
-          linkTo="/sort/countingsort"
-          imgLink={counting}
-        ></Card>
-      </div>
-      <div className="mt-8 grid gird-cols-1 md:flex gap-8 items-center justify-center">
         <Card
           title="Selection Sort"
           description="Efficient for Small Dataset, Limited memory and Real time processing."
@@ -29,6 +21,14 @@ const Sort = () => {
           imgLink={selection}
         ></Card>
       </div>
+      {/* <div className="mt-8 grid gird-cols-1 md:flex gap-8 items-center justify-center">
+        <Card
+          title="Counting Sort"
+          description="The effcicent sorting algorithm for Integer number."
+          linkTo="/sort/countingsort"
+          imgLink={counting}
+        ></Card>
+      </div> */}
     </div>
   );
 };
